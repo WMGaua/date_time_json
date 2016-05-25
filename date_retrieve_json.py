ba3442a55b927f8041eb044c70ad6023ce21ed5a
WMGaua <wmgaua@gmail.com>
2016-05-25 22:42:25 +0200

    Ver2

--- date_retrieve_json.py
+++ date_retrieve_json.py
@@ -3,15 +3,13 @@ import json
 import re
 
 
+print "The script provides current date and time in Nairobi and saves it to file in JSON format"
 url = 'http://www.timeanddate.com/worldclock/kenya/nairobi'
 content = urllib.urlopen(url).read()
 date = re.findall(r'<span .*?ctdat>(.*?)</span>', str(content))
 
 data = []
 data.append({'Nairobi date and time:': date})
-
-
-print "The scripts provides current date and time in Nairobi and saves it to file in JSON format"
 
 with open("data.json", "w") as file:
         json.dump(data, file)
